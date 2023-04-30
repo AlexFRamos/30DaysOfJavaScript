60f6c8a125b8fff77b7f467cd8ec0b3770308512
Alex Ramos <dg.alexramos@gmail.com>
2023-04-30 12:30:09 -0500

    Inicio de día 4

---
+++ Día_04/Clase/clase04.js
@@ -0,0 +1,28 @@
+// If
+    let num = 3
+    if(num>0){
+        console.log(`${num} es un número positivo`)
+    }
+
+    let isRaining =  true
+    if (isRaining) {
+        console.log("Recuerda llevar tu impermeable.")
+    }
+
+// If else
+    num = -3
+    if(num>0){
+        console.log(`${num} es un número positivo`)
+    }else {
+        console.log(`${num} es un número negativo`)
+    }
+
+    isRaining = false
+    if (isRaining) {
+        console.log("Recuerda llevar tu impermeable.")
+    } else {
+        console.log("No hay necesidad de un impermeable.")
+    }
+
+// If else if
+

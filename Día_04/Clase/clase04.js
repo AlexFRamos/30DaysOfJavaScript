db7b9bf5b4ef78825f3714f03e14c967e64faf0b
Alex Ramos <dg.alexramos@gmail.com>
2023-05-05 14:21:07 -0500

    continuando clase

--- Día_04/Clase/clase04.js
+++ Día_04/Clase/clase04.js
@@ -25,4 +25,33 @@
     }
 
 // If else if
+    let a=0;
+    if(a>0){
+        console.log(`${a} es un número positivo`);
+    } else if(a<0){
+        console.log(`${a} es un número negativo`);
+    } else if(a==0){
+        console.log(`${a} es cero`);
+    } else {
+        console.log(`${a} no es un número`);   
+    }
 
+// Switch
+    let weather = "cloudy"
+    switch (weather) {
+        case "rainy":
+            console.log("Necesitas un impreameable");
+            break;
+        case "cloudy":
+            console.log("Puede que haga frío, necesitas una chaquita");
+            break;
+        case "sunny":
+            console.log("Sal tranquilo");
+            break;
+    
+    
+        default:
+            console.log("No hay necesidad de un impermeable");
+            
+            break;
+    }

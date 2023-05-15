eb9cad8b3cfdcee61adbe28c3632b6ebf167a691
Alex Ramos <dg.alexramos@gmail.com>
2023-05-14 21:54:54 -0500

    Inicio día 5

---
+++ Día_05/Clase/clase05.js
@@ -0,0 +1,21 @@
+// Arrays
+    // Constructor de arrays
+        const arr = new Array();
+        console.log(arr);
+    // Usando corchetes []
+        const arr2= [];
+        console.log(arr2);
+    
+// Crear un array con valores
+    const numbers = [0, 3.14, 9.81, 37, 98.6, 100];
+    const fruits = ["banana", "orange", "mango", "lemon"];
+    const vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"];
+    const animalProducts = ["milk", "meat", "butter", "yoghurt"];
+    const webTechs = ["Html", "Css", "Js", "React", "Redux", "node"];
+    const countries = ["Finland", "Denmark", "Sweden", "Norway", "Icela"];
+
+// Imprimir los array y su longitud
+    console.log("Numbers: "+ numbers);
+    console.log("Numbers of numbers: "+ numbers.length);
+    
+    

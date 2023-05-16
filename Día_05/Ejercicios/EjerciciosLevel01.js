70b7341f7d507e9bcbf58e0f07342e63789fb132
Alex Ramos <dg.alexramos@gmail.com>
2023-05-15 20:59:24 -0500

    Inicio de los ejercicios del día 5

---
+++ Día_05/Ejercicios/EjerciciosLevel01.js
@@ -0,0 +1,62 @@
+const countries = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+  ];
+  
+  const webTechs = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+  ];
+
+// Declara un array vacío
+	const arregloVacío = [];
+
+// Declara un array con más de 5 elementos
+	const cincoElementos = ["Ojos", "Nariz", "Boca", "Cejas", "Cabello"];
+
+// Encuentra la longitud de tu array
+	console.log(cincoElementos.length);
+
+// Obtenga el primer elemento, el elemento del medio y el último elemento de un array
+	console.log(cincoElementos);
+	
+	const pri = cincoElementos[0];
+	const med = cincoElementos[2];
+	const ult = cincoElementos[4];
+	console.log("primero: "+pri);
+	console.log("intermedio: "+med);
+	console.log("último: "+ult);
+
+// Declara un array llamado mixedDataTypes, coloque diferentes tipos de datos en el array y encuentre la longitud del array. El tamaño del array debe ser mayor que 5.
+	const mixedDataTypes = [
+		"Pedro", 
+		2023, 
+		[1,2,3,4], 
+		"Frutas", 
+		["Leche", "Pan", "Huevos", 3000],
+		["a", "e", "i", "o", "u"], 
+		"En el bosque de la china"
+	];
+
+		console.log("Longitud del arreglo: "+ mixedDataTypes.length);
+		console.log("Arreglo: "+ mixedDataTypes);
+
+// Declare un variable array de nombre itCompanies y asignarles valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+		
+		
+
+	
